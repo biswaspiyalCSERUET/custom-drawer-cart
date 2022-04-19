69700649769384eeb785963cccad59d5608d2c59
biswaspiyalCSERUET <biswaspiyal777@gmail.com>
2022-04-19 20:32:33 +0600

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,32 @@
+import Alpine from 'alpinejs'
+import persist from '@alpinejs/persist'
+
+window.Alpine = Alpine
+Alpine.plugin(persist)
+
+Alpine.start()
+
+let shopifyCustom = {
+    updateQuantity(line, qty) {
+        fetch('/cart/change.js', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    quantity: qty,
+                    line: line
+                })
+            }).then(response => response.json())
+            .then(data => {
+                // fire javascript event on window
+                window.dispatchEvent(new Event('cart-updated'));
+
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+}
+
+window.shopifyCustom = shopifyCustom
